6ebc31eda8fd3dce65178397e29a002fa2a001ce
bozheville <bozheville06@gmail.com>
2012-08-29 19:02:22 +0300

    Поменял дефолтный абаут на какое-то дно

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -6,7 +6,22 @@ $this->breadcrumbs=array(
 	'About',
 );
 ?>
-<h1>About</h1>
+<?php
+/* @var $this SiteController */
 
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+$this->pageTitle=Yii::app()->name . ' - About';
+$this->breadcrumbs=array(
+    'About',
+);
+?>
+<style>
+    h2, p{
+        text-align: center;
+    }
+</style>
+<h2>Осталось только лишь оно:<br />Родное ебаное дно!</h2>
+<p>
+    <em>Блог творческого объединения &laquo;Дети днища, верные сыны&raquo;</em>
+</p>
+<!--<p>This is a "static" page. You may change the content of this page
+by updating the file <code><?php echo __FILE__; ?></code>.</p>-->
