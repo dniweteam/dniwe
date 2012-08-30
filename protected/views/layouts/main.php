e1f775e6781394f492ee2c83376b846ac31b35ab
bozheville <bozheville06@gmail.com>
2012-08-30 17:46:04 +0300

    Залил шрифты, Поменял главную и футер.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -2,15 +2,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="language" content="en"/>
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/fonts/stylesheet.css"/>
+    <link rel="stylesheet" type="text/css"
+          href="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/css/bootstrap.min.css"/>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/js/bootstrap.min.js"></script>
 
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
 <body>
@@ -21,31 +23,31 @@
             <div class="container">
                 <a class="brand" href="#"><?php echo CHtml::encode(Yii::app()->name); ?></a>
 
-                <?php $this->widget('zii.widgets.CMenu',array(
-                    'htmlOptions' => array('class' => 'nav',),
-                    'items'=>array(
-                        array('itemOptions' => array(
-                            'class' => 'divider-vertical',
-                        )),
-                        array('label'=>'Home', 'url'=>array('/site/index')),
-                        array('itemOptions' => array(
-                            'class' => 'divider-vertical',
-                        )),
-                        array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-                        array('itemOptions' => array(
-                            'class' => 'divider-vertical',
-                        )),
-                        array('label'=>'Contact', 'url'=>array('/site/contact')),
-                        array('itemOptions' => array(
-                            'class' => 'divider-vertical',
-                        )),
-                        array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-                        array('itemOptions' => array(
-                            'class' => 'divider-vertical',
-                        )),
-                        array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
-                    ),
-                )); ?>
+                <?php $this->widget('zii.widgets.CMenu', array(
+                'htmlOptions' => array('class' => 'nav',),
+                'items' => array(
+                    array('itemOptions' => array(
+                        'class' => 'divider-vertical',
+                    )),
+                    array('label' => 'Home', 'url' => array('/site/index')),
+                    array('itemOptions' => array(
+                        'class' => 'divider-vertical',
+                    )),
+                    array('label' => 'About', 'url' => array('/site/page', 'view' => 'about')),
+                    array('itemOptions' => array(
+                        'class' => 'divider-vertical',
+                    )),
+                    array('label' => 'Contact', 'url' => array('/site/contact')),
+                    array('itemOptions' => array(
+                        'class' => 'divider-vertical',
+                    )),
+                    array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+                    array('itemOptions' => array(
+                        'class' => 'divider-vertical',
+                    )),
+                    array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest),
+                ),
+            )); ?>
 
                 <form class="navbar-search pull-right">
                     <input type="text" class="search-query" placeholder="Search">
@@ -53,23 +55,25 @@
             </div>
         </div>
     </div>
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
+    <?php if (isset($this->breadcrumbs)): ?>
+        <?php $this->widget('zii.widgets.CBreadcrumbs', array(
+        'links' => $this->breadcrumbs,
+    )); ?><!-- breadcrumbs -->
+    <?php endif?>
 
-	<?php echo $content; ?>
+    <?php echo $content; ?>
 
-	<div class="clear"></div>
+    <div class="clear"></div>
 
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
 
-</div><!-- page -->
+    <div id="footer">
+        &copy; <?php print_r("2012" . (date('Y') > 2012 ? " &ndash; " . date('Y') : "")); ?> Твоческое объединение &laquo;Дети днища, верные сыны&raquo;<br/>
+        <?php echo "";//Yii::powered(); ?>
+    </div>
+    <!-- footer -->
+
+</div>
+<!-- page -->
 
 </body>
 </html>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,17 +4,6 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<div class="insextitle">Бложек ебаного дна</div>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<p class="ptserif"><em>Тут будет блог. Но не сейчас - потом. Сейчас тут текст &laquo;Тут будет блог. Но сейчас - потом. Сейчас тут текст &laquo;...&raquo;&raquo;</em></p>
