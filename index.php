d4a27ee7bf9007e6133a25fc63bee28e4ade7d9d
Vladimir <vmelnik@acceptic.com>
2012-09-06 11:16:34 +0300

    added framework to root folder

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__) . DIRECTORY_SEPARATOR  . '..' . DIRECTORY_SEPARATOR . 'frameworks' . DIRECTORY_SEPARATOR  . 'yii-1.1.12.b600af' . DIRECTORY_SEPARATOR  . 'framework' . DIRECTORY_SEPARATOR  . 'yii.php';
+$yii=dirname(__FILE__) . DIRECTORY_SEPARATOR  . 'framework' . DIRECTORY_SEPARATOR  . 'yii.php';
 $config=dirname(__FILE__) . DIRECTORY_SEPARATOR  . 'protected' . DIRECTORY_SEPARATOR  . 'config' . DIRECTORY_SEPARATOR  . 'main.php';
 
 // remove the following lines when in production mode
